95c8745f3f4c1c8f8769f4cb532ef5cc8fa8b501
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-07-30 05:17:17 +0300

    update

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,30 +1,42 @@
 #!/usr/bin/python3
-"""For a given employee ID, returns information about
-their TODO list progress"""
-
+"""Using this REST API, for a given employee ID,
+returns information about his/her TODO list progress."""
 import requests
 import sys
 
 if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        user_id = sys.argv[1]
 
-    userId = sys.argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                        .format(userId))
+        base_url = 'https://jsonplaceholder.typicode.com'
+        user_url = f'{base_url}/users/{user_id}'
+        todos_url = f'{base_url}/todos'
 
-    name = user.json().get('name')
+        user_response = requests.get(user_url)
+        todos_response = requests.get(todos_url)
 
-    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
-    totalTasks = 0
-    completed = 0
+        if todos_response.status_code == 200:
+            todos = todos_response.json()
+            user_todos = [todo for todo in todos
+                          if todo['userId'] == int(user_id)]
+            completed = [todo['title'] for todo in user_todos
+                         if todo['completed']]
 
-    for task in todos.json():
-        if task.get('userId') == int(userId):
-            totalTasks += 1
-            if task.get('completed'):
-                completed += 1
+            total_tasks = len(user_todos)
+            done_tasks = len(completed)
 
-    print('Employee {} is done with tasks({}/{}):'
-          .format(name, completed, totalTasks))
-
-    print('\n'.join(["\t " + task.get('title') for task in todos.json()
-          if task.get('userId') == int(userId) and task.get('completed')]))
+            if user_response.status_code == 200:
+                user_data = user_response.json()
+                employee_info = (f"Employee {user_data['name']} is done with "
+                                 f"tasks ({done_tasks}/{total_tasks}):")
+                print(employee_info)
+                for title in completed:
+                    print(f"\t  {title}")
+            else:
+                print('Failed to retrieve user data:',
+                      user_response.status_code)
+        else:
+            print('Failed to retrieve todo list data:',
+            todos_response.status_code)
+    else:
+        print('No user ID provided')
