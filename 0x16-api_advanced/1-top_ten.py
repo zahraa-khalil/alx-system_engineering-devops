03d99364515b68b24099bb81c1ad086c5956ccae
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-08-16 05:26:19 +0300

    update

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+import requests
+"""Query the Reddit API"""
+
+
+def top_ten(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'MyRedditAPI/0.1'}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            data = response.json()
+            posts = data['data']['children']
+
+            for post in posts:
+                print(post['data']['title'])
+        else:
+            print(None)
+    except requests.RequestException:
+        print(None)
