ce737538b99451e3841ae34837293c3ee525e796
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-08-16 05:23:33 +0300

    structure

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3s
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'MyRedditAPI/0.1'}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data'].get('subscribers', 0)
+        else:
+            return 0
+    except requests.RequestException:
+        return 0
