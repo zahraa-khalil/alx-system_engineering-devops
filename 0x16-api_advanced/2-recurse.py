1f23ef9b177eb10ab9bd669d5cffe9e29ede2642
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-08-16 05:30:26 +0300

    update

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""Query the Reddit API"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'MyRedditAPI/0.1'}
+    params = {'after': after, 'limit': 100}
+
+    try:
+        response = requests.get(url, headers=headers,
+                                params=params, allow_redirects=False)
+
+        if response.status_code == 200:
+            data = response.json()
+            posts = data['data']['children']
+
+            for post in posts:
+                hot_list.append(post['data']['title'])
+
+            next_after = data['data']['after']
+            if next_after:
+                return recurse(subreddit, hot_list, next_after)
+            else:
+                return hot_list
+        else:
+            return None
+    except requests.RequestException:
+        return None
